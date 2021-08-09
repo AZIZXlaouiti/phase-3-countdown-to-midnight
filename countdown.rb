04ad54b810babe5f28a66821f178da04e4f12b97
AZIZXlaouiti <azizrocke@gmail.com>
2021-08-09 13:49:09 -0400

    completed

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  while num>=1
+    puts "#{num} SECOND(S)!"
+    num-=1
+  end
+  "HAPPY NEW YEAR!"
+end
+def countdown_with_sleep(count)
+    sleep count
+    while count>=1
+       count-=1
+      end
+     
 end
